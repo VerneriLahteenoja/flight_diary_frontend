20fe26afd0dc17b3d307ebd1b29cff980107d7cc
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-07-21 18:11:47 +0300

    create DiaryEntryForm component and render it in App

---
+++ src/components/DiaryEntryForm.tsx
@@ -0,0 +1,38 @@
+// Form to add new diaries
+import { useState } from "react";
+
+const DiaryEntryForm = () => {
+  const [date, setDate] = useState('');
+  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('');
+  const [comment, setComment] = useState('');
+
+  const addEntry = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    console.log("addEntry clicked");
+    //TODO: Add form success handling
+    setDate('');
+    setVisibility('');
+    setWeather('');
+    setComment('');
+  };
+  return (
+    <form onSubmit={addEntry}>
+      <label htmlFor="date">date</label>
+      <input type="text" name="date" value={date} onChange={({target}) => setDate(target.value)}/>
+      <br />
+      <label htmlFor="visibility">visibility</label>
+      <input type="text" name="visibility" value={visibility} onChange={({target}) => setVisibility(target.value)} />
+      <br />
+      <label htmlFor="weather">weather</label>
+      <input type="text" name="weather" value={weather} onChange={({target}) => setWeather(target.value)} />
+      <br />
+      <label htmlFor="comment">comment</label>
+      <input type="text" name="comment" value={comment} onChange={({target}) => setComment(target.value)} />
+      <br />
+      <button type="submit">add</button>
+    </form>
+  );
+};
+
+export default DiaryEntryForm;
